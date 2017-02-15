c07c2c1e3cd344e31d2e52153d19c9e53e1e3290
Фирсов Никита Сергеевич <ip611s24@awp07.csc.local>
2017-02-15 13:05:32 +0700

    kokokox2

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
-#include <iostream>
+#include <stdio.h>
 using namespace std;
  
 int main()
 {
-  cout << "это моя первая программа111.";
+  prinf("это моя первая программа111.");
  
 return 0 ;
  
